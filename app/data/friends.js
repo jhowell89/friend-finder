ba87e5e7e2516130e7195e1fbc66553602a996f7
jhowell89 <53121363+jhowell89@users.noreply.github.com>
2019-12-12 20:38:37 -0500

    homework

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [ {
+    "name" : "Judy",
+    "photo" : "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Tommy",
+    "photo" : "https://images.unsplash.com/photo-1552072805-2a9039d00e57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Sebrina",
+    "photo" : "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Amy",
+    "photo" : "https://images.unsplash.com/photo-1566807810030-3eaa60f3e670?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Shawn",
+    "photo" : "https://images.unsplash.com/photo-1549068106-b024baf5062d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Billy",
+    "photo" : "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Christie",
+    "photo" : "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Doug",
+    "photo" : "https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+
+}];
+
+module.exports = friends;
+
+
