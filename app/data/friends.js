ca83ea49073ddd4c690269c372c748c847bcd825
jhowell89 <53121363+jhowell89@users.noreply.github.com>
2019-12-13 22:46:53 -0500

    homework

--- app/data/friends.js
+++ app/data/friends.js
@@ -30,6 +30,15 @@ var friends = [ {
     "name" : "Doug",
     "photo" : "https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
     "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Hannah",
+    "photo" : "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/72136068_643932202796720_6885530245680594944_n.jpg?_nc_cat=105&_nc_ohc=yVOURtlIkPQAQnEIwUJWEAJQxxmYSBpyDbXdkgoF2PIvE8qyiI9eIZ54A&_nc_ht=scontent-atl3-1.xx&oh=517a78da3183f2646e66692de71b204c&oe=5E7BD4B6",
+    "scores" : [1,4,1,3,1,4,3,4,4,5]
+}, {
+    "name" : "Cassie Hall",
+    "photo" : "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/67523390_596014690921805_2388822138149666816_n.jpg?_nc_cat=106&_nc_ohc=uEfHu07WZP8AQnr14cV3HpKLAmrMAdYI5Ghdt_5ATY4puzH_2AMnCLWsA&_nc_ht=scontent-atl3-1.xx&oh=e35565ff313f1d1a723387a6c1f372bb&oe=5E74FC36",
+    "scores" : [3,2,4,4,3,3,5,2,4,5]
+
 
 }];
 
